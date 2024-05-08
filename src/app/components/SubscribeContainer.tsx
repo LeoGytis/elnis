f5b70cbb9a8d79de9549c532b0f91a90f196ccbd
leogytis <leogytis@gmail.com>
2024-05-08 19:28:10 +0300

    full structure

--- src/app/components/SubscribeContainer.tsx
+++ src/app/components/SubscribeContainer.tsx
@@ -1,10 +1,15 @@
 export const SubscribeContainer = () => {
 	return (
-		<div className="w-full flex justify-center items-center bg-yellow-500">
-			<h1>Užsiprenumeruokit ELNIS naujienlaiškį</h1>
-			<div>Įveskite savo el pašto adresą</div>
-			<button> Prenumeruoti</button>
-			<p>
+		<div className="w-full flex flex-col justify-center items-center bg-yellow-500 p-16">
+			<h1 className="text-4xl p-8">Užsiprenumeruokit ELNIS naujienlaiškį</h1>
+			<div className="relative">
+				<input placeholder="Įveskite savo el pašto adresą" className="w-96 h-16" />
+				<button className="absolute top-0 right-[-80px] bg-orange-500 text-white border border-black rounded-full p-4">
+					{" "}
+					Prenumeruoti{" "}
+				</button>
+			</div>
+			<p className="w-1/2 text-center py-8 ">
 				Užsiprenumeruokite elnis.lt naujienlaiškį ir gaukite informaciją apie naujausius straipsnius, naujas
 				darbo pozicijas ir kt. aktualią informaciją.
 			</p>
