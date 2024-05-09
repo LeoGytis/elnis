a4b3be9835d5234ab44361a3fc83d7518c7712d9
leogytis <leogytis@gmail.com>
2024-05-09 19:38:04 +0300

    save

--- src/app/components/ContactsContainer.tsx
+++ src/app/components/ContactsContainer.tsx
@@ -3,47 +3,51 @@ import Image from "next/image";
 
 export const ContactsContainer = () => {
 	return (
-		<div className="flex justify-center border-4 border-black bg-pink-400">
-			<div className="w1/2 flex flex-col gap-4 p-16">
-				<div className="text-2xl">Kontaktai</div>
-				<div>IMAGE</div>
-				<div>IĮ "ELNIS"</div>
-				<div>
-					<p>Įmonės kodas:</p>
-					<p>304153085</p>
-				</div>
-				<div>
-					<p>PVM mokėtojo kodas:</p>
-					<p>LT100009868416</p>
-				</div>
-				<div>
-					<p>Nanko sąskaita:</p>
-					<p>LT474010051003606689</p>
-					<p>Luminor Bank AS</p>
-				</div>
-				<div className="flex gap-4 p-8">
-					<div className="p-4 bg-white rounded-full border-4 border-black">+370 623 06955</div>
-					<div className="p-4 bg-white rounded-full border-4 border-black">info@elnis.lt</div>
+		<div className="flex justify-center border-4 border-black bg-[#FF90E8]">
+			<div className="w-1/2 flex justify-center py-16">
+				<div className="flex flex-col">
+					<div className="text-[48px] text-[#333333] font-extrabold pb-20">Kontaktai</div>
+					<div className="flex flex-col gap-8 text-[28px] font-semibold pb-24">
+						<Image src="/images/brackets.png" alt="Logo" width={80} height={80} priority />
+						<div>IĮ "ELNIS"</div>
+						<div>
+							<p>Įmonės kodas:</p>
+							<p>304153085</p>
+						</div>
+						<div>
+							<p>PVM mokėtojo kodas:</p>
+							<p>LT100009868416</p>
+						</div>
+						<div>
+							<p>Banko sąskaita:</p>
+							<p>LT474010051003606689</p>
+							<p>Luminor Bank AS</p>
+						</div>
+					</div>
+					<div className="flex gap-[38px] text-[28px] ">
+						<div className="bg-white rounded-full border-4 border-black shadow-right-bottom-small px-6 py-3">
+							+370 623 06955
+						</div>
+						<div className="bg-white rounded-full border-4 border-black shadow-right-bottom-small px-6 py-3">
+							info@elnis.lt
+						</div>
+					</div>
 				</div>
 			</div>
-			<div className="w1/2 border-2 border-black bg-yellow-400 p-16 ">
-				<div className="w-96 relative">
-					<div className="absolute top-8 left-36 z-10">
-						<Image src="/images/instagram.png" alt="Logo" width={60} height={60} priority />
+			<div className="w-1/2 flex flex-col justify-center items-center border-l-4 border-black bg-[#FFD300]">
+				<div className="relative">
+					<div className="absolute top-[-20%] left-36 z-10">
+						<Image src="/images/instagram.png" alt="Logo" width={100} height={100} priority />
 					</div>
-					<div className="absolute top-40 left-0 z-10">
-						<Image src="/images/wifi.png" alt="Logo" width={60} height={60} priority />
+					<div className="absolute top-40 left-[-10%] z-10">
+						<Image src="/images/wifi.png" alt="Logo" width={100} height={100} priority />
 					</div>
-					<div className="absolute bottom-0 left-2 z-10">
-						<Image src="/images/facebook.png" alt="Logo" width={60} height={60} priority />
+					<div className="absolute bottom-[-10%] left-2 z-10">
+						<Image src="/images/facebook.png" alt="Logo" width={100} height={100} priority />
 					</div>
-
-					<div className="pt-52">
-						<Image src="/images/internet.png" alt="Logo" width={600} height={400} priority />
+					<div className="">
+						<Image src="/images/internet.png" alt="Logo" width={700} height={700} priority />
 					</div>
-					<div></div>
-					<div></div>
-					<div></div>
 				</div>
 			</div>
 		</div>
--- src/app/components/SliderContainer.tsx
+++ src/app/components/SliderContainer.tsx
@@ -3,46 +3,44 @@ import {FaRegCircle} from "react-icons/fa6";
 
 export const SliderContainer = () => {
 	return (
-		<div className="">
-			<div className="flex flex-col gap-20 justify-center items-center py-20">
-				<div className="w-full flex justify-center">
-					<Image src="/images/brands.png" alt="Logo" width={1800} height={40} priority />
+		<div className="flex flex-col gap-20 justify-center items-center py-20">
+			<div className="w-full flex justify-center">
+				<Image src="/images/brands.png" alt="Logo" width={1800} height={40} priority />
+			</div>
+			<div className="max-w-screen-xl flex gap-8">
+				<div className="w-1/3 h-[390px]  border-[3px] border-black rounded bg-[#FFC900] shadow-right-bottom-small px-[45px] py-8">
+					<h1 className="text-2xl font-bold pb-8">TANDEMO</h1>
+					<p className="pb-8">Ačiū, puikus darbas, tikiuosi daugiau ateityje!</p>
+					<p>Paulius Sanajevas</p>
+					<p>— Tandemo —</p>
 				</div>
-				<div className="max-w-screen-xl flex gap-8">
-					<div className="w-1/3 h-[390px]  border-[3px] border-black rounded bg-[#FFC900] shadow-right-bottom-small px-[45px] py-8">
-						<h1 className="text-2xl font-bold pb-8">TANDEMO</h1>
-						<p className="pb-8">Ačiū, puikus darbas, tikiuosi daugiau ateityje!</p>
-						<p>Paulius Sanajevas</p>
-						<p>— Tandemo —</p>
-					</div>
-					<div className="w-1/3 h-[390px] border-[3px] border-black rounded bg-[#90A8ED] shadow-right-bottom-small px-[45px] py-8">
-						<h1 className="text-2xl font-bold pb-8">EXTERUS</h1>
-						<p className="pb-8">Dirbti su jumis yra gera ir profesionalu. Turėsime dar daug reikalų</p>
-						<p>Audrius Valatkevičius</p>
-						<p>— Exterus —</p>
-					</div>
-					<div className="w-1/3 h-[390px] font-light border-[3px] border-black rounded bg-[#E2442F] shadow-right-bottom-small px-[45px] py-8">
-						<h1 className="text-2xl font-bold text-white pb-8">Jurgis DID</h1>
-						<p className="pb-6">
-							“Fantastiškai produktyvu, profesionalu ir operatyvu. Labiausiai žavi tai, kad kliento
-							problemos, lūkesčiai ar norai yra traktuojami, kaip savo. Taip gimsta žmogiškas ryšys bei
-							pasitikėjimas, kuris....”
-						</p>
-						<p className="text-right font-bold">SKAITYTI DAUGIAU</p>
-						<p>Kostas Kanapeckas</p>
-						<p>— Jurgis DID —</p>
-					</div>
+				<div className="w-1/3 h-[390px] border-[3px] border-black rounded bg-[#90A8ED] shadow-right-bottom-small px-[45px] py-8">
+					<h1 className="text-2xl font-bold pb-8">EXTERUS</h1>
+					<p className="pb-8">Dirbti su jumis yra gera ir profesionalu. Turėsime dar daug reikalų</p>
+					<p>Audrius Valatkevičius</p>
+					<p>— Exterus —</p>
 				</div>
-				<div className="flex items-center gap-2">
-					<FaRegCircle />
-					<div className="w-10 h-[18px] rounded-full bg-black"></div>
-					<FaRegCircle />
-					<FaRegCircle />
-					<FaRegCircle />
+				<div className="w-1/3 h-[390px] font-light border-[3px] border-black rounded bg-[#E2442F] shadow-right-bottom-small px-[45px] py-8">
+					<h1 className="text-2xl font-bold text-white pb-8">Jurgis DID</h1>
+					<p className="pb-6">
+						“Fantastiškai produktyvu, profesionalu ir operatyvu. Labiausiai žavi tai, kad kliento problemos,
+						lūkesčiai ar norai yra traktuojami, kaip savo. Taip gimsta žmogiškas ryšys bei pasitikėjimas,
+						kuris....”
+					</p>
+					<p className="text-right font-bold">SKAITYTI DAUGIAU</p>
+					<p>Kostas Kanapeckas</p>
+					<p>— Jurgis DID —</p>
 				</div>
-				<div className="w-full flex justify-center">
-					<Image src="/images/brands2.png" alt="Logo" width={1800} height={40} priority />
-				</div>
+			</div>
+			<div className="flex items-center gap-2">
+				<FaRegCircle />
+				<div className="w-10 h-[18px] rounded-full bg-black"></div>
+				<FaRegCircle />
+				<FaRegCircle />
+				<FaRegCircle />
+			</div>
+			<div className="w-full flex justify-center">
+				<Image src="/images/brands2.png" alt="Logo" width={1800} height={40} priority />
 			</div>
 		</div>
 	);
