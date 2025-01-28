04df6181cca9010f4448d93f3cd9888aeb7ef477
Gytis Leonavicius <101097792+LeoGytis@users.noreply.github.com>
2025-01-28 20:33:43 +0200

    save settings for static html

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // output: "export", // for static html build
+  //   assetPrefix: "./",   //this feature is disabled now
+  //   assetPrefix: "https://www.elnis.eu/",  //set path
+  //   distDir: "build01",  //build folder
+  // stackoverflow issue https://stackoverflow.com/questions/58784444/nextjs-export-is-broken-no-css-no-js/68691247#68691247
+};
 
 export default nextConfig;
